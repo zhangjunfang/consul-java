74d8acb6828981ad26984e4f16503bd7c75ebd18
zhangjunfang <zhangjunfang0505@163.com>
2017-03-22 14:01:56 +0800

    1

---
+++ src/main/java/com/ocean/consul/client/Test.java
@@ -0,0 +1,69 @@
+package com.ocean.consul.client;
+
+import java.util.Collections;
+import java.util.List;
+
+import com.ecwid.consul.v1.ConsulClient;
+import com.ecwid.consul.v1.QueryParams;
+import com.ecwid.consul.v1.Response;
+import com.ecwid.consul.v1.agent.model.NewService;
+import com.ecwid.consul.v1.health.model.HealthService;
+import com.ecwid.consul.v1.kv.model.GetValue;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ConsulClient client = new ConsulClient("localhost");
+
+		// set KV
+		byte[] binaryData = new byte[] {1,2,3,4,5,6,7};
+		client.setKVBinaryValue("someKey", binaryData);
+
+		client.setKVValue("com.my.app.foo", "foo");
+		client.setKVValue("com.my.app.bar", "bar");
+		client.setKVValue("com.your.app.foo", "hello");
+		client.setKVValue("com.your.app.bar", "world");
+
+		// get single KV for key
+		Response<GetValue> keyValueResponse = client.getKVValue("com.my.app.foo");
+		System.out.println(keyValueResponse.getValue().getKey() + ": " + keyValueResponse.getValue().getDecodedValue()); // prints "com.my.app.foo: foo"
+
+		// get list of KVs for key prefix (recursive)
+		Response<List<GetValue>> keyValuesReponse = client.getKVValues("com.my");
+		keyValuesReponse.getValue().forEach(value -> System.out.println(value.getKey() + ": " + value.getDecodedValue())); // prints "com.my.app.foo: foo" and "com.my.app.bar: bar"
+
+		//list known datacenters
+		Response<List<String>> response = client.getCatalogDatacenters();
+		System.out.println("Datacenters: " + response.getValue());
+
+		// register new service
+//		NewService newService = new NewService();
+//		newService.setId("myapp_01");
+//		newService.setName("myapp");
+//		newService.setTags(Arrays.asList("EU-West", "EU-East"));
+//		newService.setPort(8080);
+//		client.agentServiceRegister(newService);
+
+		// register new service with associated health check
+		NewService newService = new NewService();
+		newService.setId("myapp_02");
+		newService.setTags(Collections.singletonList("EU-East"));
+		newService.setName("myapp");
+		newService.setPort(8080);
+
+		NewService.Check serviceCheck = new NewService.Check();
+		serviceCheck.setScript("/usr/bin/some-check-script");
+		serviceCheck.setInterval("10s");
+		newService.setCheck(serviceCheck);
+
+		client.agentServiceRegister(newService);
+
+		// query for healthy services based on name (returns myapp_01 and myapp_02 if healthy)
+		Response<List<HealthService>> healthyServices = client.getHealthServices("myapp", true, QueryParams.DEFAULT);
+		healthyServices.getConsulLastContact();
+		// query for healthy services based on name and tag (returns myapp_01 if healthy)
+		//Response<List<HealthService>> healthyServices = client.getHealthServices("myapp", "EU-West", true, QueryParams.DEFAULT);
+
+	}
+
+}
